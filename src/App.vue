155677fea56901db3c6de7caea1d59be408c54e8
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2025-03-09 21:16:28 +0530

    basic todo with vue js and typescript add edit delete

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,16 @@ const addTask = (task: TaskType) => {
 const deleteTask = (taskId: number) => {
   tasks.value = tasks.value.filter((task) => task.id !== taskId);
 };
+
+const updateTask = (updatedTask:TaskType) => {
+   const index = tasks.value.findIndex((task) => task.id === updatedTask.id)
+   if(index!==-1){
+    tasks.value[index] = updatedTask
+   }
+}
 </script>
 
 <template>
   <AddTodo @add-todo="addTask" />
-  <ListTodo :tasks="tasks" @delete-Task="deleteTask" />
+  <ListTodo :tasks="tasks" @delete-Task="deleteTask"  @updated-Task="updateTask"/>
 </template>
--- src/components/ListTodo.vue
+++ src/components/ListTodo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from "vue";
+import { defineProps, defineEmits, reactive } from "vue";
 
 type TaskType = {
   id: number;
@@ -10,7 +10,30 @@ defineProps<{ tasks: TaskType[] }>();
 
 const emit = defineEmits<{
   (e: "delete-Task", taskId: number): void;
+  (e: "updated-Task", task: TaskType): void;
 }>();
+
+const editStates = reactive<
+  Record<number, { isEditing: boolean; text: string }>
+>({});
+
+const editOpen = (task: TaskType) => {
+  editStates[task.id] = { isEditing: true, text: task.text };
+};
+
+const cancelEdit = (taskId: number) => {
+  editStates[taskId].isEditing = false;
+};
+
+const saveTask = (taskId: number) => {
+  const updated = {
+    id: taskId,
+    text: editStates[taskId].text,
+  };
+  emit("updated-Task", updated);
+  editStates[taskId].isEditing = false;
+};
+
 </script>
 
 <template>
@@ -20,9 +43,23 @@ const emit = defineEmits<{
       v-for="(todo, index) in tasks"
       :key="todo.id"
     >
-      <p class="text-white">{{ index + 1 }} = {{ todo.text }}</p>
+      <input
+        v-if="editStates[todo.id]?.isEditing"
+        type="text"
+        class="py-1 px-4 outline-none border border-gray-400 rounded text-white"
+        v-model="editStates[todo.id].text"
+      />
+      <p v-else class="text-white">{{ index + 1 }} = {{ todo.text }}</p>
       <div class="flex gap-3">
-        <p class="cursor-pointer text-sm">🖋️</p>
+        <p v-show="editStates[todo.id]?.isEditing" class="cursor-pointer text-sm" @click="saveTask(todo.id)">✔️</p>
+        <p
+          v-if="editStates[todo.id]?.isEditing"
+          class="cursor-pointer text-sm text-white"
+          @click="cancelEdit(todo.id)"
+        >
+          x
+        </p>
+        <p v-else class="cursor-pointer text-sm" @click="editOpen(todo)">🖋️</p>
         <p class="cursor-pointer text-sm" @click="emit('delete-Task', todo.id)">
           ❌
         </p>
